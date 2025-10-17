8794ebe93e527d940a2571486856b2b7b9b15f66
rilantech <laodemurizal7@gmail.com>
2025-10-18 00:10:43 +0700

    Memanggil fungsi getKeuntunganTerbaik dan mengisi parameternya dengan nilai pada variabel array soal1, soal2, soal3, soal4, kemudian menampilkan hasil nya menggunakan sout.

--- src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor4.java
+++ src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor4.java
@@ -6,9 +6,14 @@ public class SoalNomor4 {
         int[] soal2 = {5, 12, 11, 12, 10};
         int[] soal3 = {7, 18, 27, 10, 29};
         int[] soal4 = {20, 17, 15, 14, 10};
+
+        System.out.println("Soal 1  : " + getKeuntunganTerbaik(soal1));
+        System.out.println("Soal 2  : " + getKeuntunganTerbaik(soal2));
+        System.out.println("Soal 3  : " + getKeuntunganTerbaik(soal3));
+        System.out.println("Soal 4  : " + getKeuntunganTerbaik(soal4));
     }
 
-    public static int getHargaBeliTerbaik(int[] harga) {
+    public static int getKeuntunganTerbaik(int[] harga) {
 
         int minHarga = harga[0];
         int maxUntung = 0;
