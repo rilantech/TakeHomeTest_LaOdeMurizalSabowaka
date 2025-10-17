c98fad4de792d8535b1b0c51e25a4265f40c2663
rilantech <laodemurizal7@gmail.com>
2025-10-17 23:42:14 +0700

    Disini saya menggunakan perulangan do while untuk membuat program yang menghasilkan deret angka sederhana yang susunan angkanya merupakan penjumlahan dari dua angka sebelumnya

--- src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor3.java
+++ src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor3.java
@@ -2,6 +2,19 @@ package com.take_home_test.laodemurizalsabowaka.jawaban_logical_test;
 
 public class SoalNomor3 {
     public static void main(String[] args) {
+        int jumlahDeret = 9;
+        int angkaPertama = 0;
+        int angkaKedua = 1;
 
+        System.out.print(angkaPertama + ", " + angkaKedua);
+
+        int i = 2;
+        do{
+            int selanjutnya = angkaPertama + angkaKedua;
+            System.out.print(", " + selanjutnya);
+            angkaPertama = angkaKedua;
+            angkaKedua = selanjutnya;
+            i++;
+        }while(i < jumlahDeret);
     }
 }
