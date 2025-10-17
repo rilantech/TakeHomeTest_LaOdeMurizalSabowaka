bd7c47cd81115e6a619855df07ce152cc53d8a36
rilantech <laodemurizal7@gmail.com>
2025-10-17 23:31:26 +0700

    Disini saya menggunakan perulangan do dan if else untuk program yang menghasilkan 100 baris angka, berurutan dari 1 sampai dengan 100. Apabila sebuah angka habis dibagi 3, maka tampilkan kata Fizz di sebelahnya.Jika angkanya habis dibagi 5, tampilkan kata Buzz di sebelahnya. Bila angka tersebut habis dibagi 3 dan habis dibagi 5, tampilkan kata FizzBuzz di sebelah angka tersebut.

--- src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor2.java
+++ src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor2.java
@@ -2,6 +2,20 @@ package com.take_home_test.laodemurizalsabowaka.jawaban_logical_test;
 
 public class SoalNomor2 {
     public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.print(i);
 
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(" FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println(" Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println(" Buzz");
+            } else {
+                System.out.println();
+            }
+            i++;
+        } while(i <= 100);
     }
 }
