4f49730c70404f4b91e201db234332cf164f82ac
rilantech <laodemurizal7@gmail.com>
2025-10-18 00:05:35 +0700

    Membuat fungsi untuk mengambil nilai saham dengan keuntungan terbaik menggunakan perulangan do while dan pengkondisian if.

--- src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor4.java
+++ src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor4.java
@@ -7,4 +7,26 @@ public class SoalNomor4 {
         int[] soal3 = {7, 18, 27, 10, 29};
         int[] soal4 = {20, 17, 15, 14, 10};
     }
+
+    public static int getHargaBeliTerbaik(int[] harga) {
+
+        int minHarga = harga[0];
+        int maxUntung = 0;
+        int keuntunganTerbaik = harga[0];
+
+        for (int i = 1; i < harga.length; i++) {
+            int untung = harga[i] - minHarga;
+
+            if (untung > maxUntung) {
+                maxUntung = untung;
+                keuntunganTerbaik = minHarga;
+            }
+
+            if (harga[i] < minHarga) {
+                minHarga = harga[i];
+            }
+        }
+
+        return keuntunganTerbaik;
+    }
 }
