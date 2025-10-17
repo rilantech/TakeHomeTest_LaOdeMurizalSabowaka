5e29d4d09162e4db2ddc185ace235d07b5db5861
rilantech <laodemurizal7@gmail.com>
2025-10-17 23:09:39 +0700

    mengerjakan soal nomor 1 yang dimulai dengan membuat fungsi untuk memperbaiki kalimat yang teracak

---
+++ src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor1.java
@@ -0,0 +1,27 @@
+package com.take_home_test.laodemurizalsabowaka.jawaban_logical_test;
+
+public class SoalNomor1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String perbaikiKalimat(String input){
+        StringBuilder hasilKonversi = new StringBuilder();
+
+        String[] lines = input.strip().split("\\R");
+        for (String line : lines) {
+            String[] words = line.trim().split(" ");
+            StringBuilder reversedLine = new StringBuilder();
+
+            for (int i = 0; i < words.length; i++) {
+                String reversedWord = new StringBuilder(words[i]).reverse().toString();
+                reversedLine.append(reversedWord);
+                if (i < words.length - 1) reversedLine.append(" ");
+            }
+            hasilKonversi.append(reversedLine).append("\n");
+        }
+
+        return hasilKonversi.toString();
+    }
+}
