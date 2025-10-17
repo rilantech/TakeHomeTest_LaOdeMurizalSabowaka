65731c40f01d97bbb8522fe9c01b21a589dfd6d5
rilantech <laodemurizal7@gmail.com>
2025-10-17 23:49:26 +0700

    Membuat variabel soal1, soal2, soal3, dan soal4 yang bertipe integer array dan langsung diisi datanya berdasarkan soal

--- src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor4.java
+++ src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor4.java
@@ -2,6 +2,9 @@ package com.take_home_test.laodemurizalsabowaka.jawaban_logical_test;
 
 public class SoalNomor4 {
     public static void main(String[] args) {
-
+        int[] soal1 = {7, 8, 3, 10, 8};
+        int[] soal2 = {5, 12, 11, 12, 10};
+        int[] soal3 = {7, 18, 27, 10, 29};
+        int[] soal4 = {20, 17, 15, 14, 10};
     }
 }
