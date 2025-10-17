9c4cc8759f1613b4dd49ecaed48e1099c8c99579
rilantech <laodemurizal7@gmail.com>
2025-10-17 22:57:50 +0700

    setup project

---
+++ src/test/java/com/take_home_test/laodemurizalsabowaka/LaodemurizalsabowakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.take_home_test.laodemurizalsabowaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaodemurizalsabowakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
