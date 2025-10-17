b1238e34a4d54fa98901d0b5d8866e9fc23297bd
rilantech <laodemurizal7@gmail.com>
2025-10-18 00:23:03 +0700

    Membuat variabel soal1, soal2, soal3 yang bertipe string array dan langsung diisi datanya dengan list string berdasarkan soal

--- src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor5.java
+++ src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_logical_test/SoalNomor5.java
@@ -2,6 +2,8 @@ package com.take_home_test.laodemurizalsabowaka.jawaban_logical_test;
 
 public class SoalNomor5 {
     public static void main(String[] args) {
-
+        String[] soal1  = {"b", "7", "h", "6", "h", "k", "i", "5", "g", "7", "8"};
+        String[] soal2  = {"7", "b", "8", "5", "6", "9", "n", "f", "y", "6", "9"};
+        String[] soal3  = {"u", "h", "b", "n", "7", "6", "5", "1", "g", "7", "9"};
     }
 }
