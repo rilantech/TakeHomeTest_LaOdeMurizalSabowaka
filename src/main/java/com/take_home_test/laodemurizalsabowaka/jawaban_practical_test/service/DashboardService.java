ebca3d51846a1bd94b45d4963b082d86837e66e9
rilantech <laodemurizal7@gmail.com>
2025-10-19 13:41:44 +0700

    Membuat class DashboardService

---
+++ src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_practical_test/service/DashboardService.java
@@ -0,0 +1,7 @@
+package com.take_home_test.laodemurizalsabowaka.jawaban_practical_test.service;
+
+import com.take_home_test.laodemurizalsabowaka.jawaban_practical_test.model.DashboardSummary;
+
+public interface DashboardService {
+    DashboardSummary getDashboardSummary(int month, int year);
+}
