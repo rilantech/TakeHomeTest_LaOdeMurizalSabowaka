3cbd3e5fcb0cfc4ad7696ed6e1fe0418680989be
rilantech <laodemurizal7@gmail.com>
2025-10-19 14:51:32 +0700

    menambah Bean restTemplate didalam class MainApp untuk melakukan HTTP request ke service eksternal, mengubah nama MainApp dan menambah dependency com.h2database pada file pom.xml agar class MainApp tidak mengalami error saat dirunning.

---
+++ src/main/java/com/take_home_test/laodemurizalsabowaka/MainApp.java
@@ -0,0 +1,21 @@
+package com.take_home_test.laodemurizalsabowaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+}
