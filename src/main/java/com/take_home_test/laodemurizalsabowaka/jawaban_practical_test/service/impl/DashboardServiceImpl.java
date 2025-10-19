a98f9672b02df538fe3c34f8829d6829c9631b64
rilantech <laodemurizal7@gmail.com>
2025-10-19 13:58:33 +0700

    Membuat class DashboardServiceImpl dan menyimpannya didalam folder impl, kemudian didalam class DashboardServiceImpl saya membuat 3 variabel yaitu restTemplate, bookingApiUrl untuk menampung api get transaksi pemesanan ruangan dan variabel konsumsiApiUrl untuk menampung api get master jenis konsumsi. Setelah itu didalam method getDashboardSummary saya melakukan saya membuat 2 variabel lagi yaitu bookings yang bertipe model Booking dan variabel masterKs yang bertipe MasterKonsumsi untuk melakukan fetching data dari api eksternal yang telah tertampung didalam variabel bookingApiUrl dan konsumsiApiUrl

---
+++ src/main/java/com/take_home_test/laodemurizalsabowaka/jawaban_practical_test/service/impl/DashboardServiceImpl.java
@@ -0,0 +1,24 @@
+package com.take_home_test.laodemurizalsabowaka.jawaban_practical_test.service.impl;
+
+import com.take_home_test.laodemurizalsabowaka.jawaban_practical_test.model.Booking;
+import com.take_home_test.laodemurizalsabowaka.jawaban_practical_test.model.DashboardSummary;
+import com.take_home_test.laodemurizalsabowaka.jawaban_practical_test.model.MasterKonsumsi;
+import com.take_home_test.laodemurizalsabowaka.jawaban_practical_test.service.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class DashboardServiceImpl implements DashboardService {
+    private final RestTemplate restTemplate;
+    private final String bookingApiUrl = "https://66876cc30bc7155dc017a662.mockapi.io/api/dummy-data/bookingList";
+    private final String konsumsiApiUrl = "https://6686cb5583c983911b03a7f3.mockapi.io/api/dummy-data/masterJenisKonsumsi";
+    @Override
+    public DashboardSummary getDashboardSummary(int month, int year) {
+        Booking[] bookings = restTemplate.getForObject(bookingApiUrl, Booking[].class);
+        MasterKonsumsi[] masterKs = restTemplate.getForObject(konsumsiApiUrl, MasterKonsumsi[].class);
+
+        return null;
+    }
+}
